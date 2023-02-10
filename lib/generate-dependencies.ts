b8550c669ed5e6b7557757f655bdbf4c643225c4
Alessandro Magionami <alessandro.magionami@gmail.com>
2023-02-10 17:16:17 +0100

    fix: remove unnecessary dependencies sorting

--- lib/generate-dependencies.ts
+++ lib/generate-dependencies.ts
@@ -3,9 +3,9 @@ interface PackageJson {
   devDependencies: Record<string, string>
 }
 
-export function generateDependencies(cliPackage: PackageJson, templateDependencies: Set<string>) {
+export function generateDependencies(cliPackage: PackageJson, templateDependencies?: Set<string>) {
   const dependencies: PackageJson['dependencies'] | PackageJson['devDependencies'] = {}
-  for (const dep of Array.from(templateDependencies).sort()) {
+  for (const dep of Array.from(templateDependencies ?? [])) {
     dependencies[dep] = cliPackage.devDependencies[dep] || cliPackage.dependencies[dep]
   }
   return dependencies
