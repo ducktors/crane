d6ededb57fb135f4fe9821e45b4cb337e4ad20e6
Maksim Sinik <maksim@sinik.it>
2023-02-12 12:06:43 +0100

    feat: add test reporting and coveralls

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    outputFile: './junit.xml',
+    coverage: {
+      all: true,
+      reportsDirectory: 'coverage',
+      reporter: ['lcovonly', 'text-summary'],
+      provider: 'istanbul',
+      include: ['**/*.{ts,tsx}'],
+    },
+  },
+})
