b86e2d314534f3ed9d945ee82318d21ee0246139
Maksim Sinik <maksim@sinik.it>
2023-03-02 17:05:09 +0100

    fix: add rome.json to every app and package in the monorepo

--- lib/renderers/lib.ts
+++ lib/renderers/lib.ts
@@ -1,6 +1,3 @@
-import { unlink } from 'node:fs/promises'
-import { join } from 'node:path'
-
 import { render } from './render'
 import { renderPackageJson } from './package-json'
 
@@ -8,7 +5,6 @@ export async function renderLib(
   templateRoot: string,
   packageName: string | undefined,
   destFolder: string,
-  projectType: 'lib' | 'monorepo-lib',
   existingProject: 'force' | 'inject' | 'skip',
 ) {
   if (existingProject !== 'inject' && packageName !== undefined) {
@@ -16,8 +12,4 @@ export async function renderLib(
   }
   render(templateRoot, 'base', destFolder, existingProject)
   render(templateRoot, 'lib', destFolder, existingProject)
-  if (projectType === 'monorepo-lib') {
-    // remove destFolder/rome.json
-    await unlink(join(destFolder, 'rome.json'))
-  }
 }
